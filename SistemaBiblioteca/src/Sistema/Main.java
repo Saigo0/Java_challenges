c3a07dca41e98e69b84cffeafcd7ad569b4379a4
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-01-12 23:23:08 -0300

    add modicações

--- SistemaBiblioteca/src/Sistema/Main.java
+++ SistemaBiblioteca/src/Sistema/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[]args) {
         Biblioteca biblioteca = Biblioteca.getInstancia("Biblioteca do Jonathan", "Rua Alameda", "11:00 - 19:00", 1000, "Pública");
-
+        
         UsuarioEspecial userEspecial = new UsuarioEspecial("Jonathan", "MG-813.834.34", "14375229632", "22/12/2004", "jonathanrezendp@gmail.com", "Rua Marcílio João da Silveira", "998574479", "DevSanttos", "Básico", "Abrhdafa", "12/12/2024", "Nenhuma", 150);
         System.out.println(userEspecial.getNivelBeneficio());
 
